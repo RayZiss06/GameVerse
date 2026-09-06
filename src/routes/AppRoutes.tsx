a86a6dbe3e107e51e33234706d33659008b24f21
omshreedev <shreeom.osj06@gmail.com>
2026-09-06 20:30:17 +0530

    updated routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,28 +3,60 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import HomePage from "../pages/HomePage";
 import UnauthorizedPage from "../pages/UnauthorizedPage";
-
+import DashboardPage from "../pages/DashboardPage";
 import { ProtectedRoute } from "./ProtectedRoute";
 
 export function AppRoutes() {
   return (
     <Routes>
-      {/* Public route */}
+      {/* Public Home Page */}
+      <Route path="/" element={<HomePage />} />
+
+      {/* Public Login Page */}
       <Route path="/login" element={<LoginPage />} />
 
-      {/* Protected routes */}
+      {/* Protected Routes */}
       <Route element={<ProtectedRoute />}>
-        <Route path="/home" element={<HomePage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        {/* other protected routes */}
       </Route>
 
       {/* Unauthorized */}
       <Route path="/unauthorized" element={<UnauthorizedPage />} />
 
-      {/* Root */}
-      <Route path="/" element={<Navigate to="/home" replace />} />
-
-      {/* Unknown routes */}
-      <Route path="*" element={<Navigate to="/home" replace />} />
+      {/* Unknown Routes */}
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
+
+// import { Navigate, Route, Routes } from "react-router-dom";
+
+// import LoginPage from "../pages/LoginPage";
+// import HomePage from "../pages/HomePage";
+// import UnauthorizedPage from "../pages/UnauthorizedPage";
+
+// import { ProtectedRoute } from "./ProtectedRoute";
+
+// export function AppRoutes() {
+//   return (
+//     <Routes>
+//       {/* Public route */}
+//       <Route path="/login" element={<LoginPage />} />
+
+//       {/* Protected routes */}
+//       <Route element={<ProtectedRoute />}>
+//         <Route path="/home" element={<HomePage />} />
+//       </Route>
+
+//       {/* Unauthorized */}
+//       <Route path="/unauthorized" element={<UnauthorizedPage />} />
+
+//       {/* Root */}
+//       <Route path="/" element={<Navigate to="/home" replace />} />
+
+//       {/* Unknown routes */}
+//       <Route path="*" element={<Navigate to="/home" replace />} />
+//     </Routes>
+//   );
+// }
